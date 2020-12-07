3d287f64e6fbc44c4e4c7e3366c7215ae6930f43
Tony <waiting33118@gmail.com>
2020-12-07 22:58:43 +0800

    feat: add checkout alias

--- The_First_Setup/git_alias.sh
+++ The_First_Setup/git_alias.sh
@@ -8,6 +8,7 @@ git config --global alias.st status
 git config --global alias.ps push
 git config --global alias.pl pull
 git config --global alias.br branch
+git config --global alias.co checkout
 git config --global alias.fc fetch
 git config --global alias.l "log --oneline --graph"
 
