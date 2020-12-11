5dc58b49fd9eddb2b8bed40772345d20fd8ebc7a
Tony <waiting33118@gmail.com>
2020-12-12 00:36:39 +0800

    feat: move setup.sh to root path

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 1. update apt and add costum repo
-./the_first_apt_update.sh
+./The_First_Setup/the_first_apt_update.sh
 
 # 2. install all packages and setup some config
-./quick_setup_env.sh
+./The_First_Setup/quick_setup_env.sh
 
 # 3. copy daily update script to /home
-sudo cp -p ./../Daily/apt_update.sh ~/apt_update.sh
-sudo cp -p ./../Daily/beforeShutdown.sh ~/beforeShutdown.sh
+sudo cp -p ./Daily/apt_update.sh ~/apt_update.sh
+sudo cp -p ./Daily/beforeShutdown.sh ~/beforeShutdown.sh
 
 # 4. create workspace directory in home directory
 mkdir ~/myWorkSpace
 
 # 5. set git alias
-./git_alias.sh
+./The_First_Setup/git_alias.sh
