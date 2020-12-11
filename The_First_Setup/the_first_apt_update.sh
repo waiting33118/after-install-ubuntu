5286c717bfe432f1d5d6d7c7dfb77472f6290ed9
Tony <waiting33118@gmail.com>
2020-12-12 00:01:38 +0800

    feat: add new ppa repo

--- The_First_Setup/the_first_apt_update.sh
+++ The_First_Setup/the_first_apt_update.sh
@@ -5,7 +5,8 @@ sudo apt update && sudo apt-get update
 
 # add apt repository
 sudo add-apt-repository universe && \
-sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:git-core/ppa && \
+sudo apt-add-repository ppa:maarten-baert/simplescreenrecorder
 
 # apt upgrade
 sudo apt upgrade -y
