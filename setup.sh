074e81b885bec8544ecc2bfa43ac331d816fe5f5
Tony Chung <waiting33118@gmail.com>
2021-03-01 23:28:46 +0800

    feat: edit setup script

--- setup.sh
+++ setup.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+##############################################################
+# Quick setup environment script for new installed Ubuntu PC #
+##############################################################
+
 # 1. update apt and add costum repo
-./The_First_Setup/the_first_apt_update.sh
+./scripts/the_first_apt_update.sh
 
 # 2. install all packages and setup some config
-./The_First_Setup/quick_setup_env.sh
+./scripts/quick_setup_env.sh
 
-# 3. copy daily update script to /home
-sudo cp -p ./Daily/apt_update.sh ~/apt_update.sh
-sudo cp -p ./Daily/beforeShutdown.sh ~/beforeShutdown.sh
+# 3. copy daily script to user's bin folder
+sudo cp -p ./Daily/daily-update ./Daily/before-shutdown /usr/local/bin
 
 # 4. create workspace directory in home directory
 mkdir ~/myWorkSpace
 
 # 5. set git alias
-./The_First_Setup/git_alias.sh
+./scripts/git_alias.sh
 
 # 6. install node.js & packages
-./The_First_Setup/nvm_setup.sh
+./scripts/nvm_setup.sh
