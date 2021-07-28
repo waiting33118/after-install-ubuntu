882ef64bfb47ec3d3984e60eaf3b403b0af1b668
Tony Chung <waiting33118@gmail.com>
2021-07-28 22:48:39 +0800

    feat: add new ppa

--- scripts/the_first_apt_update.sh
+++ scripts/the_first_apt_update.sh
@@ -6,7 +6,8 @@ sudo apt update && sudo apt-get update
 # add apt repository
 sudo add-apt-repository universe && \
 sudo add-apt-repository ppa:git-core/ppa && \
-sudo apt-add-repository ppa:maarten-baert/simplescreenrecorder
+sudo apt-add-repository ppa:maarten-baert/simplescreenrecorder && \
+sudo add-apt-repository ppa:remmina-ppa-team/remmina-next
 
 # apt upgrade
 sudo apt upgrade -y
